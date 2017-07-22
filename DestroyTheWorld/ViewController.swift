c3647e5cbb24c6b7c1c59d7a4646ef8ba1c3a3f9
Alex Alajbegovic <alexalajbegovic@Alexs-MacBook-Pro-2.local>
2017-07-22 10:18:50 -0700

    Hopefully trhis works again

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Alex Alajbegovic on 7/22/17.
+//  Copyright © 2017 Alex Alajbegovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
